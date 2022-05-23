08a9ec7a16c917bfafc89367aa0dfb40c3692c35
Balicea28 <b.alicea1998@gmail.com>
2022-05-23 14:40:19 -0400

    commit tree 127032ff985d4c533e849d97bff3367ba7a1ce92

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(rideLength){
+  if(rideLength <= 400) {
+    return 'This one is on me!';
+  }
 
-function ternaryCheckCity(){
-  // Write your code here!
+  if (rideLength >400){
+    return 'That will be twenty bucks.';
+  } else if (rideLength >=2000){
+    return 'That will be twenty bucks.'
+  }
+  
+  function ternaryCheckCity(city){
+    return city === 'NYC' ? 'Ok, sounds good.': 'No go.';
 }
 
 function switchOnCharmFromTip(){
-  // Write your code here!
+}
+
 }
